84f9553785f3a2de07ed438916992655f0b177eb
Imdimark <78663960+Imdimark@users.noreply.github.com>
2023-08-04 15:33:51 +0200

    Add files via upload

---
+++ my-app2/my-app2/src/main/java/com/mycompany/app/Model/IPerson.java
@@ -0,0 +1,5 @@
+package com.mycompany.app.Model;
+
+public interface IPerson {
+    String FullName();
+}
---
+++ my-app2/my-app2/src/main/java/com/mycompany/app/Model/Manager.java
@@ -0,0 +1,84 @@
+package com.mycompany.app.Model;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.xml.bind.annotation.XmlElement;
+
+//@XmlRootElement(name = "manager")
+
+public class Manager extends Person {
+    private static int counter = 500;
+    //List<Person> persons;
+    public Manager(String name, String surname, String department) {
+        
+        super(name,surname);
+        this.department = department;
+        this.id = ++counter;
+        this.persons = new ArrayList<>();
+    }
+    
+    
+    @XmlElement(name = "name")
+    protected String name;
+
+    @XmlElement(name = "surname")
+    protected String surname;
+    
+    public Manager() {
+        // Costruttore senza argomenti
+    }
+    
+
+    @XmlElement(name = "person")
+    private List<Person> persons;
+
+    @XmlElement(name = "department")
+    private String department;
+
+    public void SetId(int idd){        
+        this.id = idd;        
+    }
+
+    /*public void AddEmployeeInList(Employee employee){
+        this.persons.add(employee);
+
+    }*/
+
+    public void AddPersonInList(Person person){
+        this.persons.add(person);
+
+    }
+
+    public void RemovePersonInList(Person person){
+        this.persons.remove(person);
+    }
+
+    public List<Person> GetPersons()
+    {
+        return this.persons;
+    }
+
+    @Override
+    public String FullName() {
+        return String.format("%1$s %2$s (%3$s)", super.name,super.surName,this.department);
+    }
+
+    public int GetId() {
+        return id;
+    }
+
+    public void printManager(int level) {
+        for (int i = 0; i < level; i++) {
+          System.out.print("\t");
+        }        
+        System.out.println("Manager: " + name);
+        for (Object subordinate : persons) {
+          if (subordinate instanceof Manager) {
+            ((Manager) subordinate).printManager(level + 1);
+          } else if (subordinate instanceof Employee) {
+            ((Employee) subordinate).printEmployee(level + 1);
+          }
+        }  
+      }
+  
+}
