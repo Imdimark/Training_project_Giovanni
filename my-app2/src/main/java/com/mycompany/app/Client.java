b4bc4204595851b2a204d6ba52a0832d8b7fd066
Imdimark <78663960+Imdimark@users.noreply.github.com>
2023-08-28 17:19:39 +0200

    Add files via upload

--- my-app2/src/main/java/com/mycompany/app/Client.java
+++ my-app2/src/main/java/com/mycompany/app/Client.java
@@ -7,28 +7,31 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 import com.mycompany.app.BusinnesLogic.Controller;
+import com.mycompany.app.Exceptions.IdAlreadyValorized;
+import com.mycompany.app.Exceptions.ManagerHasPersonsInList;
+import com.mycompany.app.Exceptions.ManagerNotFound;
+import com.mycompany.app.Exceptions.TryingAssignEmployeeAsManager;
 import com.mycompany.app.Model.Company;
 import com.mycompany.app.Model.Employee;
 import com.mycompany.app.Model.Manager;
-import com.mycompany.app.Model.Person;
 
 public class Client {
-        public static void main( String[] args ) throws FileNotFoundException  
+        public static void main( String[] args ) throws FileNotFoundException, IdAlreadyValorized, ManagerNotFound, TryingAssignEmployeeAsManager, ManagerHasPersonsInList  
         {
             File file = new File("Company.xml"); 
             Company engineering;
+            Controller controller = null;;
             if(file.exists() ){
                 System.out.println(file + " Exists");
                 XMLDecoder decoder = new XMLDecoder(new BufferedInputStream(new FileInputStream("Company.xml")));
                 engineering = (Company)decoder.readObject();
                 decoder.close();
+                 controller =new Controller(engineering);
 
             }else{
                 System.out.println(file + " Does not exists");
-                engineering = new Company();               
-            }
-            
-                Controller controller =new Controller(engineering);
+                engineering = new Company();                    
+                controller =new Controller(engineering);
                 Manager Vincenzo= new Manager("Vincenzo","b","cybersec");
                 Manager Mario= new Manager("Mario","b","cybersec");
                 Manager Venere = new Manager("Venere","b","Sviluppo");
@@ -37,16 +40,14 @@ public class Client {
                 Employee Giovanni = new Employee("GIovanni", "mario", "devops");
                 System.out.println( Vincenzo.FullName() );
                 System.out.println( Vincenzo.getPersons() );
-                //controller.AddManager(Mariolino);
-                controller.AddManager(Vincenzo);        
+                controller.AddManager(Vincenzo); 
                 controller.AddManager(Mario, Vincenzo.getId()); 
                 controller.AddManager (Venere, Mario.getId());
-                Person gaetano = controller.AddEmployee(Gaetano, Vincenzo.getId());        
-                controller.DeletePerson(gaetano.getId());
-                controller.AddEmployee(pippo, Vincenzo.getId());       
-                controller.AddEmployee(Giovanni, Mario.getId());      
-                System.out.println(controller.printManager(Vincenzo.getId() )); 
-                controller.test(Venere);                
+                controller.AddEmployee(Gaetano, Vincenzo.getId());        
+                controller.AddEmployee(Giovanni, Mario.getId()); 
+                controller.AddEmployee(pippo, Vincenzo.getId()); 
+                System.out.println(controller.printManager(Vincenzo.getId() ));   
                 controller.Save();         
+            }                      
         }
 }
